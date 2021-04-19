86f51c089a479eae90e5b259b0ef8b3c520b4397
pishchulenok <64210553+sirejja@users.noreply.github.com>
2021-04-20 00:24:32 +0300

    Update storage.py
    Optimization of code

--- key-values_storage/storage.py
+++ key-values_storage/storage.py
@@ -1,44 +1,55 @@
 import argparse
 import json
 import os
-import sys
 import tempfile
 
-argparser = argparse.ArgumentParser()
-argparser.add_argument('--key')
-argparser.add_argument('--val')
-args = argparser.parse_args()
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+def read_data(storage_path):
+    if not os.path.exists(storage_path):
+        return {}
 
-if os.path.isfile(storage_path): 
-    if args.val:
-        with open(storage_path, "r") as f:
-            storage = json.load(f)
-            if args.key in storage:
-                storage[args.key] = storage[args.key] + [args.val]
-            else:
-                storage.update({args.key: [args.val]})
-        with open(storage_path, "w") as f:
-            json.dump(storage, f)
+    with open(storage_path, 'r') as file:
+        raw_data = file.read()
+        if raw_data:
+            return json.loads(raw_data)
+        return {}
+
+
+def write_data(storage_path, data):
+    with open(storage_path, 'w') as f:
+        f.write(json.dumps(data))
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--key', help='Key')
+    parser.add_argument('--val', help='Value')
+    return parser.parse_args()
+
+
+def put(storage_path, key, value):
+    data = read_data(storage_path)
+    data[key] = data.get(key, list())
+    data[key].append(value)
+    write_data(storage_path, data)
+
+
+def get(storage_path, key):
+    data = read_data(storage_path)
+    return data.get(key, [])
+
+
+def main(storage_path):
+    args = parse()
+
+    if args.key and args.val:
+        put(storage_path, args.key, args.val)
+    elif args.key:
+        print(*get(storage_path, args.key), sep=', ')
     else:
-        try:
-            with open(storage_path, "r") as f:
-                storage = json.load(f)
-                if storage[args.key] == None:
-                    print(None)
-                if len(storage[args.key]) > 1:
-                    print(', '.join(storage.get(args.key)))
-                else:
-                    print(*storage.get(args.key))
-        except:
-            print(None)
-else:
-    d = {}
-    with open(storage_path, "w") as f:
-        if args.val:
-            d = {args.key: [args.val]}
-            json.dump(d, f)
-        else:
-            d = {args.key: None}
-            print(None)
+        print('The program is called with invalid parameters.')
+
+
+if __name__ == '__main__':
+    storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+    main(storage_path)
